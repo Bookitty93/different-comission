1fa9a3a82966db0389be5b42fcff529b942af795
Anvar Mubashirov <anvar.mubashirov@mail.ru>
2023-03-31 19:27:41 +0500

    Change calculateCommission

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,13 @@
 fun main() {
-    val result = calculateCommission("VK PAY", 0, 200_000)
+    val result = calculateCommission("VkPAY", 0, 100_000)
     println("Комиссия составит $result рублей")
 }
 
-fun calculateCommission(cardType: String, pastTransfersAmount: Int, transferAmount: Int): Int {
-    val total = transferAmount + pastTransfersAmount
-    return if (total > 75_000) (userCard(cardType, pastTransfersAmount, transferAmount).toInt()) else 0
-}
-
-fun userCard(cardType: String, pastTransfersAmount: Int, transferAmount: Int) = when(cardType) {
-    "VK PAY" -> 0.0
-    "Mastercard" -> ((transferAmount + pastTransfersAmount) * 0.006) + 20
-    "MAESTRO" -> ((transferAmount + pastTransfersAmount) * 0.006) + 20
-    else -> 0.0
+fun calculateCommission(cardType: String, pastTransfersAmount: Int, transferAmount: Int) = when (cardType) {
+    "VkPAY" -> 0
+    "Mastercard", "MAESTRO" -> if ((transferAmount + pastTransfersAmount) > 75_000)
+        (((transferAmount + pastTransfersAmount) * 0.006) + 20).toInt() else 0
+    "Visa", "МИР" -> if ((transferAmount + pastTransfersAmount) * 0.0075 > 35)
+        ((transferAmount + pastTransfersAmount) * 0.0075).toInt() else 35
+    else -> 0
 }
