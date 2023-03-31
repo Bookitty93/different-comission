c0c584fafa31a24e24c16718986ed6ede9cae1aa
Anvar Mubashirov <anvar.mubashirov@mail.ru>
2023-03-31 20:53:03 +0500

    Final Change calculateCommission

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,29 @@
 fun main() {
-    val result = calculateCommission("VkPAY", 0, 100_000)
-    println("Комиссия составит $result рублей")
+    val result = calculateCommission()
+    println(result)
 }
 
-fun calculateCommission(cardType: String, pastTransfersAmount: Int, transferAmount: Int) = when (cardType) {
-    "VkPAY" -> 0
-    "Mastercard", "MAESTRO" -> if ((transferAmount + pastTransfersAmount) > 75_000)
-        (((transferAmount + pastTransfersAmount) * 0.006) + 20).toInt() else 0
-    "Visa", "МИР" -> if ((transferAmount + pastTransfersAmount) * 0.0075 > 35)
-        ((transferAmount + pastTransfersAmount) * 0.0075).toInt() else 35
-    else -> 0
-}
+fun calculateCommission(cardType: String = "МИР", pastTransfersAmount: Int = 0, transferAmount: Int = 15200) =
+    when (cardType) {
+        "VkPAY" -> when {
+            pastTransfersAmount + transferAmount > 40_000 -> "Превышен месячный лимит переводов"
+            transferAmount > 15_000 -> "Превышен суточный лимит переводов"
+            else -> "Комиссия составит 0 рублей"
+        }
+
+        "Mastercard", "MAESTRO" -> when {
+            transferAmount > 150_000 -> "Превышен суточный лимит переводов"
+            transferAmount + pastTransfersAmount > 600_000 -> "Превышен месячный лимит переводов"
+            transferAmount + pastTransfersAmount > 75_000 -> "Комиссия составит ${((transferAmount * 0.006) + 20).toInt()} рублей"
+            else -> "Комиссия составит 0 рублей"
+        }
+
+        "Visa", "МИР" -> when {
+            transferAmount > 150_000 -> "Превышен суточный лимит переводов"
+            transferAmount + pastTransfersAmount > 600_000 -> "Превышен месячный лимит переводов"
+            transferAmount * 0.0075 > 35 -> "Комиссия составит ${(transferAmount * 0.0075).toInt()} рублей"
+            else -> "Комиссия составит 35 рублей"
+        }
+
+        else -> 0
+    }
